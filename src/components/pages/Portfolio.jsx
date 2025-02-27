627bbef794e34352bae72c38867b8343de52b6d4
Rohit <rohit.sangod74@gmail.com>
2025-02-28 01:27:39 +0530

    3ed chang

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -25,12 +25,12 @@ function Portfolio() {
           commitment to clean code, responsive design, and user experience
         </p>
 
-        <div className="portfolio-projects border border-blue-700 w-full h-full flex gap-7 px-20 max-sm:px-5  max-sm:flex max-sm:flex-col  border-none  pb-20 max-sm:py-10">
+        <div className="portfolio-projects border border-blue-700 w-full h-full flex gap-7 px-20 max-sm:px-5  max-sm:flex max-sm:flex-col    pb-10 max-sm:py-10">
           <div className="border border-red-800 w-full border-none bg-black rounded-t-md ">
-            <div className="project flex justify-between items-center p-2 bg-blue-500 rounded-t-md">
-              <p className="font-semibold text-white">See more</p>
+            <div className="project flex justify-between items-center p-2 bg-blue-500 rounded-t-md max-sm:p-1 ">
+              <p className="font-semibold text-white  px-3">See more</p>
               {/* <p>About Project</p> */}
-              <div className="icone border-2  text-2xl rounded-full p-2 text-white mr-2 max-sm:my-2 ">
+              <div className="icone border-2  text-2xl rounded-full p-2   text-white mx-3 max-sm:my-2 max-sm:p-1 ">
                 <Icon icon="solar:course-up-broken" />
               </div>
             </div>
@@ -39,12 +39,12 @@ function Portfolio() {
               src="./images/Screenshot 2025-01-21 192715.png"
             />
           </div>
-          <div className="border border-red-800 w-full border-none bg-black rounded-t-md max-sm:px-2">
-            <div className="p-2 flex justify-between items-center pr-5 bg-blue-500 rounded-t-md ">
-              <p className="font-semibold text-white">See more</p>
+          <div className="border border-red-800 w-full border-none bg-black rounded-t-md ">
+            <div className="p-2 flex justify-between items-center  bg-blue-500 rounded-t-md max-sm ">
+              <p className="font-semibold text-white px-3">See more</p>
 
               {/* <p>About Project</p> */}
-              <div className="icone border-2 text-2xl rounded-full p-2 text-white  max-sm:my-2 ">
+              <div className="icone border-2 text-2xl rounded-full p-2 max-sm:p-1 text-white max-sm:mx-3 max-sm:my- ">
                 <Icon icon="solar:course-up-broken" />
               </div>
             </div>
@@ -53,6 +53,15 @@ function Portfolio() {
               src="./images/Screenshot 2025-01-18 113351.png"
             />
           </div>
+        </div>
+        <div className="Portfoio-Btn-Section-Main p-2 border border-black flex justify-end w-full px-20 max-sm:px-2">
+           <div className="flex justify-center items-center gap-">
+           <button>Show all Project</button>
+          
+           <div className="portfolio-btn ">
+            <Icon icon="line-md:play-filled" />
+           </div>
+           </div>
         </div>
       </div>
     </div>
